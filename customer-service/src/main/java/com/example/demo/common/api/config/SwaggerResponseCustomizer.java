2c6ee223915903c598cd39dc882f5dbef5bb8001
barissayli <baris.sayli@venhancer.com>
2025-08-26 15:56:52 -0600

    customer-service initial commit

---
+++ customer-service/src/main/java/com/example/demo/common/api/config/SwaggerResponseCustomizer.java
@@ -0,0 +1,50 @@
+package com.example.demo.common.api.config;
+
+import io.swagger.v3.oas.models.media.*;
+import org.springdoc.core.customizers.OpenApiCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static com.example.demo.common.api.config.OpenApiSchemas.*;
+
+@Configuration
+public class SwaggerResponseCustomizer {
+
+    @Bean
+    public OpenApiCustomizer responseEnvelopeSchemas() {
+        return openApi -> {
+            if (!openApi.getComponents().getSchemas().containsKey(SCHEMA_API_RESPONSE)) {
+                openApi.getComponents().addSchemas(
+                        SCHEMA_API_RESPONSE,
+                        new ObjectSchema()
+                                .addProperty(PROP_STATUS, new IntegerSchema().format("int32"))
+                                .addProperty(PROP_MESSAGE, new StringSchema())
+                                .addProperty(
+                                        PROP_ERRORS,
+                                        new ArraySchema().items(
+                                                new ObjectSchema()
+                                                        .addProperty(PROP_ERROR_CODE, new StringSchema())
+                                                        .addProperty(PROP_MESSAGE, new StringSchema())
+                                        ))
+                );
+            }
+
+            if (!openApi.getComponents().getSchemas().containsKey(SCHEMA_API_RESPONSE_VOID)) {
+                openApi.getComponents().addSchemas(
+                        SCHEMA_API_RESPONSE_VOID,
+                        new ObjectSchema()
+                                .addProperty(PROP_STATUS, new IntegerSchema().format("int32"))
+                                .addProperty(PROP_MESSAGE, new StringSchema())
+                                .addProperty(PROP_DATA, new ObjectSchema())
+                                .addProperty(
+                                        PROP_ERRORS,
+                                        new ArraySchema().items(
+                                                new ObjectSchema()
+                                                        .addProperty(PROP_ERROR_CODE, new StringSchema())
+                                                        .addProperty(PROP_MESSAGE, new StringSchema())
+                                        ))
+                );
+            }
+        };
+    }
+}
---
+++ customer-service/src/main/java/com/example/demo/customer/CustomerServiceApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+@SpringBootApplication
+@ConfigurationPropertiesScan(basePackages = "com.example.demo")
+public class CustomerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerServiceApplication.class, args);
+    }
+}
